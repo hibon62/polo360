3e00e15d909266912aab6133758e0a916f8b777f
francois <hibon.francois@gmail.com>
2016-04-13 22:11:08 +0200

    First commit
    Ajout des fichiers concrete5 qui va permettre d'installer concrete

---
+++ elements/header.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Polo 360</title>
+        <link rel="stylesheet" href="<?php echo $this->getThemePath(); ?>css/style.css" />
+        <link rel="stylesheet" href="<?php echo $this->getThemePath(); ?>css/960_12_col.css" />
+        <link rel="stylesheet" href="<?php echo $this->getThemePath(); ?>css/reset.css" />
+        <link rel="stylesheet" href="<?php echo $this->getThemePath(); ?>css/text.css" />
+    </head>
+    <body>
+        <div id="wraper">
+            <header>
+                <div class="header_cont">
+                    <div class="separater_hd"></div>
+                    <div class="clear"></div>
+                    <a href="#" class="logo"><img src="img/logo.png"></a>
+                    <nav>
+                        <ul>
+                            <li><a href="#">Home<span>Back to home</span></a></li>
+                            <li><a href="#">Product<span>What we have for you</span></a></li>
+                            <li><a href="#">Services<span>Things we do</span></a></li>
+                            <li><a href="#">Blog<span>Follow our updates</span></a></li>
+                            <li><a href="#">Contact<span>Ways to reach us</span></a></li>
+                        </ul>
+                    </nav>
+                </div>
+            </header>
